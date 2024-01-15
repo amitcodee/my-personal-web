6846288b7d4aa41479a8af15872d5e3ae5ce2b95
Amit Sharma <pandit143amit@gmail.com>
2024-01-15 19:59:50 +0530

    update

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <nav className="bg-gray-700 p-4 fixed w-full top-0">
+    <ul className="flex space-x-4">
+      <li className="text-white">Home</li>
+      <li className="text-white">About</li>
+      <li className="text-white">Projects</li>
+      <li className="text-white">Contact</li>
+    </ul>
+  </nav>
+  );
+}
+
+export default Header;
