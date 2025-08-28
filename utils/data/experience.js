0930123c7beadc90b72a189f2b8b6642345cd195
Amit Sharma <pandit143amit@gmail.com>
2025-08-28 17:35:00 +0530

    update

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,25 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer',
-    company: "CCraft Solution Private Ltd.",
-    duration: "(Jan 2023 - Present)"
+    title: 'It Representative',
+    company: "Techcadd Computer Education Pvt. Ltd.",
+    duration: "(Feb 2024 - Present)"
   },
+  
   {
     id: 2,
     title: 'FullStack Developer',
     company: "CodeGenus Software  Solution Solution Private Ltd.",
-    duration: "(Jan 2023 - Dec 2023)"
+    duration: "(June 2023 - Dec 2023)"
   },
   {
     id: 3,
+    title: 'Software Engineer',
+    company: "CCraft Solution Private Ltd.",
+    duration: "( 2022 - 2023)"
+  },
+  {
+    id: 4,
     title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "Always"
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Amit Sharma",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "My name is Amit Sharma. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I'm Amit Sharma, a passionate Software Developer and Digital Marketing professional with 3+ years of experience building responsive web applications and business solutions. I specialize in JavaScript, PHP, HTML, CSS, and modern frameworks, with proven success in developing production-ready applications like e-commerce platforms and business management softwareAs both a developer and educator, I've mentored 50+ students while maintaining a 95% satisfaction rate. I love solving complex problems, exploring new technologies, and creating solutions that make businesses more efficient. Currently pursuing my BTech in Computer Science and available for challenging development opportunities that match my full-stack expertise.",
   email: 'akamitt.kumar@gmail.com',
   phone: '+91 7710575371',
   address: 'Talwara, Punjab, India',
